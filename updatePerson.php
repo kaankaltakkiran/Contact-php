438b483e73ca8943b28e291fda61e3e285270e90
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-11-02 15:08:59 +0300

    update profil photo

--- updatePerson.php
+++ updatePerson.php
@@ -13,7 +13,23 @@
         $number = $_POST['number'];
         $id    = $_GET['id'];
 
-        $sql = "UPDATE users SET username = :name, phonenumber = :number WHERE userid = :id";
+        $img_name = $_FILES['image']['name'];
+        $img_size = $_FILES['image']['size'];
+        $tmp_name = $_FILES['image']['tmp_name'];
+      /* 	$error = $_FILES['image']['error']; */
+      
+        
+            $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+            $img_ex_lc = strtolower($img_ex);
+      
+            $allowed_exs = array("jpg", "jpeg", "png"); 
+      
+            if (in_array($img_ex_lc, $allowed_exs)) 
+              $new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
+              $img_upload_path = 'images/'.$new_img_name;
+              move_uploaded_file($tmp_name, $img_upload_path);
+
+        $sql = "UPDATE users SET username = :name, phonenumber = :number,userimg='$new_img_name' WHERE userid = :id";
         $SORGU = $DB->prepare($sql);
 
         $SORGU->bindParam(':name',  $name);
@@ -50,9 +66,10 @@
   <body>
   <h1>Record Update</h1>
 
-<form method='POST'>
+<form method='POST' enctype="multipart/form-data">
     <p>Name:  <input type='text' name='name'  value='<?php echo $users[0]['username'];  ?>'></p>
     <p>Phone Number: <input type='text' name='number' value='<?php echo$users[0]['phonenumber']; ?>'></p>
+    <p>Image: <input type='file' name='image'></p> 
     <p><input type='submit' value='Update'></p>
 </form>
 
