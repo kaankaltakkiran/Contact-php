8e7fedce910d9e98725202d2ef7ea1e7b9f10de8
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-11-02 17:29:52 +0300

    added databales dynamic

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 require 'db.php';
 ?>
-
-
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,6 +8,7 @@ require 'db.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Contact</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="//cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css" rel="stylesheet">
   </head>
   <body>
     <div class="container">
@@ -56,6 +53,13 @@ require 'db.php';
 <a type="button" href="insertPerson.php" class="btn btn-primary">Add Person</a>
 </p>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" ></script>
+  <script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+  <script>
+  $(document).ready( function () {
+		$('.table').DataTable();
+  });
+  </script>
   </body>
 </html>
